cff004fc34b0b2988e80622ff9e55203de3070b5
vagrant <vagrant@vagrant.vm>
2020-10-29 01:00:32 +0000

    added movies. Need to check query

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 """Models for movie ratings app."""
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime 
 
 db = SQLAlchemy()
 
@@ -43,7 +44,7 @@ class Rating(db.Model):
     rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     score = db.Column(db.Integer)
     movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
-    user_id = db.Column(db.integer, db.ForeignKey('users.user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
     movie = db.relationship('Movie', backref='ratings')
     user = db.relationship('User', backref='ratings')
